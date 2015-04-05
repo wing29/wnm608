82c32952cee36eecaf0a8f1394be97a2870b1ba5
wing29 <wing_29@hotmail.com>
2015-04-04 19:20:43 -0700

    Added update form function

--- week9/student_list.php
+++ week9/student_list.php
@@ -13,18 +13,44 @@
 			<?php
 				$filename = 'students.json';
 				$file = file_get_contents($filename);
-				$data = json_decode($file);
 
-				if(!isset($_GET['s'])){
+                if (isset($_GET['modify'])) {
+                    $data = json_decode($file, true);
+                } else {
+                    $data = json_decode($file);
+                }
+
+                if(isset($_GET['modify'])) {
+                    $index = intval($_GET['modify']);
+                    if (isset($_POST['name'])) {
+                        $data[$index]['name'] = $_POST['name'];
+                    }
+                    if (isset($_POST['major'])) {
+                        $data[$index]['major'] = $_POST['major'];
+                    }
+                    if (isset($_POST['marriages'])) {
+                        $data[$index]['marriages'] = $_POST['marriages'];
+                    }
+                    $ret = file_put_contents($filename, json_encode($data));
+
+                    if ($ret == false) {
+                        echo "<div>Failed to update ".$data[$index]['name']."</div>";
+                    } else {
+                        echo "<div>Updated ".$data[$index]['name']."</div>";
+                    }
+                }
+				else if (!isset($_GET['s'])) {
                     echo "<ul>";
 					foreach($data as $key=>$val){
 						printStudent($key,$val,true);
 					}
                     echo"</ul>";
-				}else{
+				} else {
+                    echo "<form id='updateForm' action='student_list.php?modify=".$_GET['s']."' method='post'>";
 					printStudent($_GET['s'],$data[$_GET['s']]);
-                    echo "<button><a href='#'>Edit</a></button>";
+                    echo "<button form='updateForm' type='submit'>Update</button>";
 					echo "<button><a href='?'>Back</a></button>";
+                    echo "</form>";
 				}
 				function printStudent($index,$student,$isInList=false){
                     if ($isInList) {
@@ -35,9 +61,9 @@
                         if (isset($student->marriages)) {
                             echo "<th>Marriages</th>"."</tr>";
                         }
-                        echo "<tr>"."<td>".$student->name."</td>"."<td>".$student->major."</td>";
+                        echo "<tr>"."<td><input class='input' name='name' value='$student->name'></td>"."<td><input class='input' name='major' value='$student->major'></td>";
                         if (isset($student->marriages)) {
-                            echo "<td>".$student->marriages."</td>";
+                            echo "<td><input class='input' name='marriages' value='$student->marriages'></td>";
                         }
                         echo "</table>";
                     }
